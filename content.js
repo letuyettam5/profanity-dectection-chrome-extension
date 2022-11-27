63b134167e337b0c2cf932311c5e1ede5f4f88f3
letuyettam5 <60398019+letuyettam5@users.noreply.github.com>
2022-11-27 18:10:13 -0500

    Add files via upload

--- content.js
+++ content.js
@@ -1,49 +1,94 @@
-check(document.body);//this line runs the function
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
 
-//this alters the document's body by running check() function
-document.body.addEventListener('DOMSubtreeModified', function () {
-   check(document.body);
-});
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
 
-function check(node)
-{
-	var child; //variable to manipulate document fragment's child
-    var next;
+var comments, rBool, pBool, hBool, sBool, vBool;
+var racism = ["nigg", "chink", "redneck", "gypsy", "redskin"];
+var profanity = ["fuck", "ass", "shit", "bastard", "cunt", "dick", "crap", "bitch", "whore", "slut", "hoe", "hell", "retard", "inbred"];
+var homophobia = ["fag", "dyke", "transvestite", "tranny", "sodomite", "homo"];
+var sexism = ["feminazi", "femenazi", "bitch", "slut", "cunt", "whore", "hoe"];
+var violence = ["kill", "death", "blood", "flesh", "gore"];
 
-	switch ( node.nodeType )
-	{
-		case Node.ELEMENT_NODE:
-		case Node.DOCUMENT_NODE:
-		case Node.DOCUMENT_FRAGMENT_NODE:
-			child = node.firstChild;
-			while ( child )
-			{
-				next = child.nextSibling;
-				check(child);
-				child = next;
+rBool = 1;
+pBool = 1;
+hBool = 1;
+sBool = 1;
+vBool = 1;
+
+
+setInterval(function() {
+	comments = document.getElementsByClassName("comment-renderer-text-content");
+	var numCensored = 0;
+	for(var i = 0; i < comments.length; i++) {
+		var censoredYet = 0;
+		if(rBool == 1) {
+			for(var j = 0; j < racism.length; j++) {
+				if(comments[i].innerHTML.toLowerCase().indexOf(racism[j]) != -1) {
+					censoredYet = 1;
+					comments[i].innerHTML = "This comment was suspended because it was deemed racist. Found a variation of: \"" + racism[j] + "\"";
+					comments[i].style = "color: #CE0D00; font-weight: bold";
+				}
 			}
-			break;
-
-		case Node.TEXT_NODE:
-			handleText(node);
-			break;
+		}
+		if(pBool == 1) {
+			for(var j = 0; j < profanity.length; j++) {
+				if(comments[i].innerHTML.toLowerCase().indexOf(profanity[j]) != -1) {
+					censoredYet = 1;
+					comments[i].innerHTML = "This comment was suspended because it was deemed profane. Found a variation of: \"" + profanity[j] + "\"";
+					comments[i].style = "color: #CE0D00; font-weight: bold";
+				}
+			}
+		}
+		if(hBool == 1) {
+			for(var j = 0; j < homophobia.length; j++) {
+				if(comments[i].innerHTML.toLowerCase().indexOf(homophobia[j]) != -1) {
+					censoredYet = 1;
+					comments[i].innerHTML = "This comment was suspended because it was deemed homophobic. Found a variation of: \"" + homophobia[j] + "\"";
+					comments[i].style = "color: #CE0D00; font-weight: bold";
+				}
+			}
+		}
+		if(sBool == 1) {
+			for(var j = 0; j < sexism.length; j++) {
+				if(comments[i].innerHTML.toLowerCase().indexOf(sexism[j]) != -1) {
+					censoredYet = 1;
+					comments[i].innerHTML = "This comment was suspended because it was deemed sexist. Found a variation of: \"" + sexism[j] + "\"";
+					comments[i].style = "color: #CE0D00; font-weight: bold";
+				}
+			}
+		}
+		if(vBool == 1) {
+			for(var j = 0; j < violence.length; j++) {
+				if(comments[i].innerHTML.toLowerCase().indexOf(violence[j]) != -1) {
+					censoredYet = 1;
+					comments[i].innerHTML = "This comment was suspended because it was deemed violent. Found a variation of: \"" + violence[j] + "\"";
+					comments[i].style = "color: #CE0D00; font-weight: bold";
+				}
+			}
+		}
+		if(censoredYet == 1) {
+			numCensored++;
+		}
 	}
-}
-
-var badWords = ["asshole","bint","bitch","bollocks","bullshit","feck","munter","pissed"
-,"shit","son of a bitch","tits","nigga", "chink", "redneck", "gypsy", "redskin",
-"whore", "slut", "hoe","fuck","bastard","dick","dickhead","pussy"];
-
-function handleText(textNode)
-{
-	var v = textNode.nodeValue;
-    v = v.replace(/\bBitch\b/g, "CENSORED");
-    // for (var i = 0; i < badWords.length; i++) {
-    //     if (v.toLowercase().indexOf(badWords[i]) != -1) {
-    //         v = "CENSORED";
-    //         //v.innerHTML.style = "color: #CE0D00; font-weight: bold";
-    //     }
-    // }
-	
-	textNode.nodeValue = v;
-}
+	if(numCensored != 0) {
+		document.title = "CENSORED: " + numCensored;
+	}
+}, 250);
