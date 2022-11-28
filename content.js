907a277e32fe7c29d7ca35fb2fc3de3492f0eb85
letuyettam5 <60398019+letuyettam5@users.noreply.github.com>
2022-11-27 21:51:07 -0500

    commit

--- content.js
+++ content.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 check(document.body);//this line runs the function
 
 //this alters the document's body by running check() function
@@ -46,4 +47,49 @@ function handleText(textNode)
     // }
 	
 	textNode.nodeValue = v;
+=======
+check(document.body);//this line runs the function
+
+//this alters the document's body by running check() function
+document.body.addEventListener('DOMSubtreeModified', function () {
+   check(document.body);
+});
+
+function check(node)
+{
+	var child; //variable to manipulate document fragment's child
+    var next;
+
+	switch ( node.nodeType )
+	{
+		case Node.ELEMENT_NODE:
+		case Node.DOCUMENT_NODE:
+		case Node.DOCUMENT_FRAGMENT_NODE:
+			child = node.firstChild;
+			while ( child )
+			{
+				next = child.nextSibling;
+				check(child);
+				child = next;
+			}
+			break;
+
+		case Node.TEXT_NODE:
+			handleText(node);
+			break;
+	}
+}
+
+function handleText(textNode)
+{
+	var v = textNode.nodeValue;
+	v = v.replace(/\bArse\b/g, "CENSORED");
+	v = v.replace(/\bBloody\b/g, "CENSORED");
+	v = v.replace(/\bBugger\b/g, "CENSORED");
+	v = v.replace(/\bCow\b/g, "CENSORED");
+	v = v.replace(/\bCrap\b/g, "CENSORED");
+	v = v.replace(/\bDamn\b/g, "CENSORED");
+	
+	textNode.nodeValue = v;
+>>>>>>> ba10408 (stuffs)
 }
